a7675549eb9a10634f30fab300882dcc1966eec6
Taylor Gleason <taylor@buzzedtop.com>
2025-10-29 23:31:51 -0400

    fixing another oversight

--- python/src/radar_range_equation/__init__.py
+++ python/src/radar_range_equation/__init__.py
@@ -3,20 +3,12 @@
 from .main import vars, \
                     equations, \
                     solve, \
-                    convert_to_degrees, \
-                    convert_to_db, \
-                    convert_m_to_mi, \
-                    convert_w_to_kw, \
-                    convert_ft_to_m, \
+                    convert, \
                     redefine_variable
 
 __all__ = ["vars",
            "equations",
            "solve",
-           "convert_to_degrees",
-           "convert_to_db",
-           "convert_m_to_mi",
-           "convert_w_to_kw",
-           "convert_ft_to_m",
+           "convert",
            "redefine_variable"
            ]
